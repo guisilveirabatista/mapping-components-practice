dd58e2892204b72dadf8a71901898a30067e6975
guisilveirabatista <40640798+guisilveirabatista@users.noreply.github.com>
2021-02-25 11:26:40 +0100

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Entry from "./Entry";
+import emojipedia from "../emojipedia.js";
+
+function createEntry(emoji) {
+  return (
+    <Entry
+      key={emoji.id}
+      id={emoji.id}
+      emoji={emoji.emoji}
+      name={emoji.name}
+      meaning={emoji.meaning}
+    />
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+      <dl className="dictionary">{emojipedia.map(createEntry)}</dl>
+    </div>
+  );
+}
+
+export default App;
